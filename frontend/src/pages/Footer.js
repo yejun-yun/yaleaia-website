80aa8b9f3c9ac8d370b498a1250ff63d7263156a
Yejun <yejunyun@Yejuns-MBP.wireless.yale.internal>
2025-08-19 10:50:38 -0700

    removed yea subsidiary

--- frontend/src/pages/Footer.js
+++ frontend/src/pages/Footer.js
@@ -37,8 +37,7 @@ function Footer() {
         </div>
       </div>
       <div className="footer-bottom">
-        <p> Subsidiary of Yale Effective Altruism. 
-          This website is published by Yale College students and Yale University is not responsible for its contents. The activities on this website are not supervised or endorsed by Yale and information contained on this website does not necessarily reflect the opinions or official positions of the University. All rights are reserved to Yale University for the Yale name and trademark. </p>
+        <p> This website is published by Yale College students and Yale University is not responsible for its contents. The activities on this website are not supervised or endorsed by Yale and information contained on this website does not necessarily reflect the opinions or official positions of the University. All rights are reserved to Yale University for the Yale name and trademark. </p>
       </div>
     </footer>
   );
