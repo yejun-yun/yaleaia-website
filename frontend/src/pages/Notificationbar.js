da7369287088e19e0fe5c43fd6e0e8421e23a92e
Yejun <yejunyun@Yejuns-MBP.wireless.yale.internal>
2025-08-25 17:14:11 -0400

    added intro fellowship banner

--- frontend/src/pages/Notificationbar.js
+++ frontend/src/pages/Notificationbar.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import '../styles/Navbar.css';
+import '../styles/Notificationbar.css';
 
 function Notificationbar() {
-  return null;
+  return (
+    <div className="notificationbar">
+      <div className="notificationbar-content">
+        <span>Apply now for the Fall '25 Intro Fellowship!</span>
+        <a
+          className="notificationbar-link"
+          href="https://docs.google.com/forms/d/e/1FAIpQLScT6ig0l_TrJuYeR2ae4_9oCzWZXaeEdHCzF7WFqUi25HCiyw/viewform?usp=dialog"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Apply
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Notificationbar
