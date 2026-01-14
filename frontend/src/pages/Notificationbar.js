5b09086687d1992f696fe0fff77893b77826b0b7
Yejun <yejunyun@Yejuns-MBP.wireless.yale.internal>
2026-01-14 12:03:58 -0500

    update banner

--- frontend/src/pages/Notificationbar.js
+++ frontend/src/pages/Notificationbar.js
@@ -5,10 +5,10 @@ function Notificationbar() {
   return (
     <div className="notificationbar">
       <div className="notificationbar-content">
-        <span>Apply now for the Fall '25 Intro Fellowship!</span>
+        <span>Apply now for the Spring '26 Intro Fellowship!</span>
         <a
           className="notificationbar-link"
-          href="https://docs.google.com/forms/d/e/1FAIpQLScT6ig0l_TrJuYeR2ae4_9oCzWZXaeEdHCzF7WFqUi25HCiyw/viewform?usp=dialog"
+          href="https://forms.gle/rMbm7rfHfFFx1mek6"
           target="_blank"
           rel="noopener noreferrer"
         >
